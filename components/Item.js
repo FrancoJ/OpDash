9e10c08c888aef4cfa3c5f44dbd08c5c70eb2525
FrancoJ <FrancoJ@users.noreply.github.com>
2017-07-13 14:32:34 -0700

    item page

--- components/Item.js
+++ components/Item.js
@@ -3,28 +3,9 @@ import fetch from 'isomorphic-unfetch';
 
 const ItemView = (props) => (
   <section>
-    <h1>Batman TV Shows</h1>
-    <ul>
-      {props.shows.map(({show}) => (
-        <li key={show.id}>
-          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
-            <a>{show.name}</a>
-          </Link>
-        </li>
-      ))}
-    </ul>
+ fdsafsdfas
   </section>
 )
 
-ItemView.getInitialProps = async function() {
-  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
-  const data = await res.json();
-
-  console.log(`Show data fetched. Count: ${data.length}`);
-
-  return {
-    shows: data
-  }
-}
 
 export default ItemView
