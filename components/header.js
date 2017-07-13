407f067cd2438028764d214cbe9d43e23a43c5bd
FrancoJ <FrancoJ@users.noreply.github.com>
2017-07-13 11:30:45 -0700

    Integrate bootstrap theme

---
+++ components/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head'
+export default () => (
+  <header>
+    <Head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Bootstrap Theme Example</title>
+    <link href="/static/bootstrap-theme/dist/toolkit-inverse.min.css" rel="stylesheet"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="/static/bootstrap-theme/dist/toolkit.min.js"></script>
+    </Head>
+  </header>
+)
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Header from '../components/header';
+
+
+const Index = () => (
+  <main>
+  	<Header />
+    <h1>Hackaton Projectzz</h1>
+
+  </main>
+)
+
+export default Index
